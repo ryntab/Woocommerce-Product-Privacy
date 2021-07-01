87090eda6798713b7fb537f59cb479b2bd24b205
ryntab <57367794+ryntab@users.noreply.github.com>
2021-07-01 03:02:55 -0400

    Added product visibility options 😋

--- admin/class-woocommerce-product-privacy-admin.php
+++ admin/class-woocommerce-product-privacy-admin.php
@@ -84,25 +84,41 @@ class woo_product_privacy_Admin
 	 *
 	 * @since    1.0.0
 	 */
-    public function product_box_display($post){
-		$state = woo_product_privacy_Admin::product_visibility_get_state($post);
+	public function product_box_display($post)
+	{
+		$privacy_options = get_option('woocommerce_product_privacy_option_name');
+		$reversal = ($privacy_options['reverse_mode'] == 'reverse_mode') ? false : true;
+		$state = woo_product_privacy_Admin::product_visibility_get_state($post, $reversal);
 		$button;
 		$button .= '<label>';
-		$button .= '<input name="product_visibility" type="checkbox" class="checkbox" '. $state .'>';
-		$button .= 'Prevent Public Access';
+		$button .= '<input type="hidden" value="off" name="'. $state['name'].'" class="checkbox" ' . $state['status'] . '>';
+		$button .= '<input type="checkbox" value="on" name="'. $state['name'].'" class="checkbox" ' . $state['status'] . '>';
+		$button .= $state['label'];
 		$button .= '</label>';
-		echo $button;
-    }
+		echo $button;	
+	}
 
 	/**
 	 * Save product visibility toggle state.
 	 *
 	 * @since    1.0.0
 	 */
-	public function product_visibility_save_state($post_id){
+	public function product_visibility_save_state($post_id)
+	{
+		if($_POST["product_visibility_reversal"] != null){
+			$key = 'product_visibility_reversal';
+			$value = $_POST["product_visibility_reversal"];
+		}
+		if($_POST["product_visibility"] != null){
+			$key = 'product_visibility';
+			$value = $_POST["product_visibility"];
+		}
+
+		if ($key == null) return;
+
 		$post_type = get_post_type($post_id);
-		if($post_type == 'product') {
-			update_post_meta($post_id,'product_visibility', $_POST["product_visibility"]);
+		if ($post_type == 'product') {
+			update_post_meta($post_id, $key, $value);
 		}
 	}
 
@@ -111,24 +127,16 @@ class woo_product_privacy_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function product_visibility_get_state($post){
-		$state = get_product($post->ID);
-		$state = get_post_meta($post->ID, 'product_visibility','true');
-		$state = ($state == 'on') ? 'checked' : '';
+	public function product_visibility_get_state($post, $reversal)
+	{
+		$visibility = ($reversal) ? 'product_visibility' : 'product_visibility_reversal';
+		$state = get_post_meta($post->ID, $visibility, 'true');
+		$state = array(
+			"status" => ($state == 'on') ? 'checked' : '',
+			"name" => ($reversal) ? 'product_visibility' : 'product_visibility_reversal',
+			"label" => ($reversal) ? 'Prevent Public Access' : 'Allow Public Access',
+		);
 		return $state;
-	}
-
-	/**
-	 * Set redirect for product page loading.
-	 *
-	 * @since    1.0.0
-	 */
-	public function product_visibility_load_action(){
-		global $post;
-		$state = get_post_meta($post->ID, 'product_visibility', 'true');
-		if ($state == 'on' && !current_user_can('administrator')){
-			woo_product_privacy_Admin::product_force_four_oh_four();
-		}
 	}
 
 	/**
@@ -136,11 +144,174 @@ class woo_product_privacy_Admin
 	 *
 	 * @since    1.0.0
 	 */
-	public function product_force_four_oh_four(){
-		global $wp_query; //$posts (if required)
+	public function product_force_four_oh_four()
+	{
+		global $wp_query;
 		$wp_query->set_404();
-    	status_header( 404 );
-    	nocache_headers();
+		status_header(404);
+		nocache_headers();
 	}
 
+
+	/**
+	 * Set redirect for product page loading.
+	 *
+	 * @since    1.0.0
+	 */
+	public function product_visibility_load_action()
+	{
+		global $post;
+		$post_type = get_post_type($post->id);
+
+		if ($post_type != 'product') return;
+
+		$privacy_options = get_option('woocommerce_product_privacy_option_name');
+
+		if ($privacy_options['hide_everything'] == 'hide_everything'){
+			woo_product_privacy_Admin::product_force_four_oh_four();
+			return;
+		}
+
+		if ($privacy_options['reverse_mode'] == 'reverse_mode'){
+			$meta = get_post_meta($post->ID, 'product_visibility_reversal', 'true');
+			if ($meta == 'on') return;
+			woo_product_privacy_Admin::product_force_four_oh_four();
+			return;
+		} 
+	
+		$state = get_post_meta($post->ID, 'product_visibility', 'true');
+		if ($state == 'on' && !current_user_can('administrator')) {
+			woo_product_privacy_Admin::product_force_four_oh_four();
+		}
+	}
 }
+
+
+
+class WoocommerceProductPrivacy
+{
+	private $woocommerce_product_privacy_options;
+
+	public function __construct()
+	{
+		add_action('admin_menu', array($this, 'woocommerce_product_privacy_add_plugin_page'));
+		add_action('admin_init', array($this, 'woocommerce_product_privacy_page_init'));
+	}
+
+	public function woocommerce_product_privacy_add_plugin_page()
+	{
+		add_options_page(
+			'Woocommerce Product Privacy', // page_title
+			'Woocommerce Product Privacy', // menu_title
+			'manage_options', // capability
+			'woocommerce-product-privacy', // menu_slug
+			array($this, 'woocommerce_product_privacy_create_admin_page') // function
+		);
+	}
+
+	public function woocommerce_product_privacy_create_admin_page()
+	{
+		$this->woocommerce_product_privacy_options = get_option('woocommerce_product_privacy_option_name'); ?>
+
+		<div class="wrap">
+			<?php settings_errors(); ?>
+
+			<h2>Woocommerce Product Privacy</h2>
+			<form method="post" action="options.php">
+				<?php
+				settings_fields('woocommerce_product_privacy_option_group');
+				do_settings_sections('woocommerce-product-privacy-admin');
+				submit_button();
+				?>
+			</form>
+		</div>
+<?php }
+
+	public function woocommerce_product_privacy_page_init()
+	{
+		register_setting(
+			'woocommerce_product_privacy_option_group', // option_group
+			'woocommerce_product_privacy_option_name', // option_name
+			array($this, 'woocommerce_product_privacy_sanitize') // sanitize_callback
+		);
+
+		add_settings_section(
+			'woocommerce_product_privacy_setting_section', // id
+			'Settings', // title
+			array($this, 'woocommerce_product_privacy_section_info'), // callback
+			'woocommerce-product-privacy-admin' // page
+		);
+
+		add_settings_field(
+			'reverse_mode', // id
+			'Reverse Mode', // title
+			array($this, 'reverse_mode_callback'), // callback
+			'woocommerce-product-privacy-admin', // page
+			'woocommerce_product_privacy_setting_section' // section
+		);
+
+		add_settings_field(
+			'hide_everything', // id
+			'Hide Everything! 💣', // title
+			array($this, 'hide_everything_callback'), // callback
+			'woocommerce-product-privacy-admin', // page
+			'woocommerce_product_privacy_setting_section' // section
+		);
+
+		add_settings_field(
+			'allow_traffic_from_referral_urls_2', // id
+			'Allow traffic from referral URLs', // title
+			array($this, 'allow_traffic_from_referral_urls_2_callback'), // callback
+			'woocommerce-product-privacy-admin', // page
+			'woocommerce_product_privacy_setting_section' // section
+		);
+	}
+
+	public function woocommerce_product_privacy_sanitize($input)
+	{
+		$sanitary_values = array();
+		if (isset($input['reverse_mode'])) {
+			$sanitary_values['reverse_mode'] = $input['reverse_mode'];
+		}
+
+		if (isset($input['hide_everything'])) {
+			$sanitary_values['hide_everything'] = $input['hide_everything'];
+		}
+
+		if (isset($input['allow_traffic_from_referral_urls_2'])) {
+			$sanitary_values['allow_traffic_from_referral_urls_2'] = esc_textarea($input['allow_traffic_from_referral_urls_2']);
+		}
+
+		return $sanitary_values;
+	}
+
+	public function woocommerce_product_privacy_section_info()
+	{
+	}
+
+	public function reverse_mode_callback()
+	{
+		printf(
+			'<input type="checkbox" name="woocommerce_product_privacy_option_name[reverse_mode]" id="reverse_mode" value="reverse_mode" %s> <label for="reverse_mode">Instead of selecting which products to hide, select which products to show.</label>',
+			(isset($this->woocommerce_product_privacy_options['reverse_mode']) && $this->woocommerce_product_privacy_options['reverse_mode'] === 'reverse_mode') ? 'checked' : ''
+		);
+	}
+
+	public function hide_everything_callback()
+	{
+		printf(
+			'<input type="checkbox" name="woocommerce_product_privacy_option_name[hide_everything]" id="hide_everything" value="hide_everything" %s> <label for="hide_everything">No products will be publicly accessible by their direct links. This will override any rules set on individual products.</label>',
+			(isset($this->woocommerce_product_privacy_options['hide_everything']) && $this->woocommerce_product_privacy_options['hide_everything'] === 'hide_everything') ? 'checked' : ''
+		);
+	}
+
+	public function allow_traffic_from_referral_urls_2_callback()
+	{
+		printf(
+			'<textarea class="large-text" rows="5" name="woocommerce_product_privacy_option_name[allow_traffic_from_referral_urls_2]" id="allow_traffic_from_referral_urls_2">%s</textarea>',
+			isset($this->woocommerce_product_privacy_options['allow_traffic_from_referral_urls_2']) ? esc_attr($this->woocommerce_product_privacy_options['allow_traffic_from_referral_urls_2']) : ''
+		);
+	}
+}
+if (is_admin())
+	$woocommerce_product_privacy = new WoocommerceProductPrivacy();
--- public/class-woocommerce-product-privacy-public.php
+++ public/class-woocommerce-product-privacy-public.php
@@ -53,49 +53,4 @@ class woo_product_privacy_Public {
 		$this->version = $version;
 
 	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in woo_product_privacy_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The woo_product_privacy_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in woo_product_privacy_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The woo_product_privacy_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-
-	}
-
 }
